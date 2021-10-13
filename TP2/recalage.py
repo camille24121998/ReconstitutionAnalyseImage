954340f4671e2a6296151c5e18eda988803b289a
pedc3102 <camille.pedron@usherbrooke.ca>
2021-10-13 14:54:20 -0400

    Ouverture image

--- TP2/recalage.py
+++ TP2/recalage.py
@@ -1,3 +1,15 @@
 import numpy as np
 import nibabel as nib
 import matplotlib.pyplot as plt
+
+def JoinHist(I, J, bin) :
+    print("test")
+    image1 = plt.imread(I)
+    plt.imshow(image1)
+    plt.show()
+
+def main():
+    JoinHist("Data/I5.jpg", "I6.jpg", 30)
+
+if __name__ == "__main__":
+    main()
