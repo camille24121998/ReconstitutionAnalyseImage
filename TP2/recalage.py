86d5d942f8e5b18e02d175af9c22a117f905a65a
menm3008 <manina.meng@usherbrooke.ca>
2021-10-14 18:07:30 +0200

    structuration + ajout d'agrument

--- TP2/recalage.py
+++ TP2/recalage.py
@@ -1,6 +1,13 @@
 import numpy as np
 import nibabel as nib
 import matplotlib.pyplot as plt
+import imageio
+import argparse
+import glob
+
+########################################
+#   Partie 1 : Histogramme conjoint    #
+########################################
 
 def JoinHist(I, J, bin) :
     print("test")
@@ -8,7 +15,34 @@ def JoinHist(I, J, bin) :
     plt.imshow(image1)
     plt.show()
 
+########################################
+#   Partie 4 : Recalage iconique 2D    #
+########################################
+
+
+
+
+############
+#   Main   #
+############
+def parse_args():
+    p = argparse.ArgumentParser(description=__doc__,
+                                formatter_class=argparse.RawTextHelpFormatter)
+    p.add_argument('path_images',
+                   help="Dossier contenant les fichiers.")
+    """
+    p.add_argument('nb_bins', type=int,
+                   help="Nombre de bins dans l'histogramme conjoint.")
+    """
+    arguments = p.parse_args()
+
+    return arguments
+
 def main():
+    args = parse_args()
+    filename_i = glob.glob(args.path_images + "I{}.*".format(1))
+    img_i = imageio.imread(filename_i[0])
+
     JoinHist("Data/I5.jpg", "I6.jpg", 30)
 
 if __name__ == "__main__":
