9f4f33c9160f5d69dd58f9676d3d77d2e0b90b6d
pedc3102 <camille.pedron@usherbrooke.ca>
2021-10-17 23:58:21 -0400

    Exercice 2 Question a et b

--- TP2/main.py
+++ TP2/main.py
@@ -1,19 +1,19 @@
 import numpy as np
 import nibabel as nib
-import matplotlib as mpl
 import matplotlib.pyplot as plt
 import argparse
+import math
 from os import listdir
 from os.path import isfile, join
 
 import RecalageIconique
 import TransformationSpatiale
 
-########################################
-#   Partie 1 : Histogramme conjoint    #
-########################################
+########################################################################
+#   Fonction auxiliere d'affichage d'image et de test de dimensions    #
+########################################################################
 
-def JoinHist(I, J, bin) :
+def transformeImageIntoMatrix(I, J) :
     plot1 = plt.figure(1)
     image1 = plt.imread(I)
     plt.imshow(image1)
@@ -22,32 +22,26 @@ def JoinHist(I, J, bin) :
     image2 = plt.imread(J)
     plt.imshow(image2)
 
-    print(image1)
-    print(np.shape(image1))
-    print(image2)
-    print(np.shape(image2))
-
-    sameDim = False
-    if(np.shape(image1) != np.shape(image2)) :
-        sameDim = True
-    if(np.size(image1)>np.size(image2) and np.size(image1)==(np.shape(image1)[2]*np.size(image2))) :
-        sameDim = True
-    if(np.size(image2)>np.size(image1) and np.size(image2)==(np.shape(image2)[2]*np.size(image1))) :
-        sameDim = True
-
-    if(sameDim == False) :
+    sameDim = np.shape(image1) == np.shape(image2)
+    if sameDim == False :
         print("ERREUR : Les images ne sont pas de la même taille, l'histogramme conjoint ne peut pas être réaliser")
-    else :
-        image1Copy = image1.flatten()
-        image2Copy = image2.flatten()
-        '''tuples = (np.array((image1, image2)).T)[0]
-        print(tuples)'''
+
+    return (sameDim, image1.flatten(), image2.flatten())
+
+########################################
+#   Partie 1 : Histogramme conjoint    #
+########################################
+
+def JoinHist(I, J, bin) :
+    (sameDim, image1Copy, image2Copy,) = transformeImageIntoMatrix(I, J)
+
+    if(sameDim == True) :
         indexes = []
         x = []
         y = []
         z = []
 
-        while(len(image1Copy) != 0 or len(image2Copy) != 0) :
+        while(len(image1Copy) != 0 and len(image2Copy) != 0) :
             val1 = image1Copy[0]
             indexes1 = np.where(image1Copy == val1)[0]
             val2 = image2Copy[0]
@@ -63,13 +57,49 @@ def JoinHist(I, J, bin) :
         plot3 = plt.figure(3)
         ax = plt.axes()
         ax.set_facecolor('#000090')
-        print(max(z))
         plt.scatter(x, y, s=1, c=z, cmap='rainbow')
         plt.title('Histogramme conjoint')
         plt.xlabel('Image 1')
         plt.ylabel('Image 2')
 
-        plt.show()
+        return (x, y, z)
+
+##########################################
+#   Partie 2 :  Critère de similarité    #
+##########################################
+
+def SSD(I, J) :
+    (sameDim, image1Copy, image2Copy,) = transformeImageIntoMatrix(I, J)
+
+    if(sameDim == True) :
+        image1Copy = image1.flatten()
+        image2Copy = image2.flatten()
+        ssd = (np.sum(image1Copy) - np.sum(image2Copy))**2
+        print("SSD = ", ssd)
+        return ssd
+
+def CR(I, J) :
+    (sameDim, image1Copy, image2Copy,) = transformeImageIntoMatrix(I, J)
+
+    if(sameDim == True) :
+        moyI1 = np.average(image1Copy)
+        moyI2 = np.average(image2Copy)
+        diffAlaMoyI1 = np.subtract(image1Copy, moyI1)
+        diffAlaMoyI2 = np.subtract(image2Copy, moyI2)
+        numerateur = np.sum(diffAlaMoyI1*diffAlaMoyI2)
+        varianceI1 = math.sqrt(np.sum(np.power(diffAlaMoyI1, 2)))
+        varianceI2 = math.sqrt(np.sum(np.power(diffAlaMoyI2, 2)))
+        denominateur = varianceI1*varianceI2
+        cr = numerateur/denominateur
+        print("Coefficient de corelation : ", cr)
+        return cr
+
+def IM(I, J) :
+    (sameDim, image1Copy, image2Copy,) = transformeImageIntoMatrix(I, J)
+
+    if(sameDim == True) :
+        (x, y, h) = JoinHist(I, J, 100)
+
 
 ############
 #   Main   #
@@ -92,10 +122,20 @@ def main():
 
     images = [args.path_images+f for f in listdir(args.path_images) if isfile(join(args.path_images, f))]
 
-    if args.question == "1a":
+    if args.question == "1":
         #I2/J2 ok, BrainMRI_1/BrainMRI_2/BrainMRI_3/BrainMRI_4 ok, I3/J3 ok, I4/J4 ok, I5/J5 ok, I6/J6 ok
         #I1/J1 pas de la même taille (512, 512, 4) et (512, 512)
-        JoinHist("Data/I2.jpg", "Data/J2.jpg", 30)
+        JoinHist("Data/I2.jpg", "Data/J2.jpg", 100)
+        plt.show()
+
+    if args.question == "2a" :
+        SSD("Data/I2.jpg", "Data/J2.jpg")
+
+    if args.question == "2b" :
+        CR("Data/I2.jpg", "Data/J2.jpg")
+
+    if args.question == "2c" :
+        IM("Data/I2.jpg", "Data/J2.jpg")
 
     if args.question == "3a":
         x, y, z = (20, 20, 4)
